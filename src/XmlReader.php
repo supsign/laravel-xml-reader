d47ca858c9e880ab1140a8ee11f0482559595390
Florian Ratz <florian.ratz@supsign.ch>
2020-12-07 08:25:09 +0100

    no message

--- src/XmlReader.php
+++ src/XmlReader.php
@@ -2,8 +2,58 @@
 
 namespace Supsign\LaravelXmlReader;
 
-use Supsign\LaravelXmlReader\DirectoryReader;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use XML;
 
-class XmlReader extends DirectoryReader {
+class XmlReader {
 
+	protected 
+		$data = null,
+		$dataKey = null,
+		$sourceFile = null,
+		$sourceFolder = 'imports';
+
+	public function getData()
+	{
+		if (!$this->dataKey) {
+			return $this->readFile();
+		}
+
+		foreach ($this->readFile() AS $key => $value) {
+			if ($key !== $this->dataKey) {
+				continue;
+			}
+
+			return $value;
+		}
+	}
+
+	protected function readFile()
+	{
+		if (!$this->sourceFile OR $this->getFileType() !== 'xml') {
+			throw new BadRequestException('file not found');
+		}
+
+		return XML::import(Storage::path($this->sourceFolder.'/'.$this->sourceFile))->collect();
+	}
+
+	public function setSourceFile($fileName) 
+	{
+		$this->sourceFile = $fileName;
+
+		return $this;
+	}
+
+	public function setSourceFolder($folderName) 
+	{
+		$this->sourceFolder = $folderName;
+
+		return $this;
+	}
+
+	protected function getFileType()
+	{
+		return strtolower(substr($this->sourceFile, strrpos($this->sourceFile, '.') + 1));
+	}
 }
